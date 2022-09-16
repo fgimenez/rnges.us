09b06ee00a2b2a1a64cc1e5cff71163492100a10
Federico Gimenez <federico.gimenez@gmail.com>
2022-09-16 12:07:37 +0200

    update libp2p

--- wasm-net/src/lib.rs
+++ wasm-net/src/lib.rs
@@ -7,29 +7,31 @@ use std::task::{Context, Poll};
 use std::time::Duration;
 
 use futures::prelude::*;
-use libp2p::gossipsub::protocol::MessageId;
-use libp2p::gossipsub::{GossipsubEvent, GossipsubMessage, Topic};
-use libp2p::wasm_ext;
-use libp2p::{gossipsub, identity, PeerId};
-use libp2p::{mplex, secio, yamux, Transport};
+use libp2p::core::transport::OptionalTransport;
+use libp2p::gossipsub::{
+    GossipsubEvent, GossipsubMessage, IdentTopic as Topic, MessageAuthenticity, MessageId,
+    ValidationMode,
+};
+use libp2p::swarm::SwarmEvent;
+use libp2p::{
+    core, gossipsub, identity, mplex, noise, wasm_ext, yamux, Multiaddr, PeerId, Transport,
+};
 
 #[cfg(not(target_os = "unknown"))]
 use libp2p::{dns, tcp, websocket};
 
 #[cfg(not(target_os = "unknown"))]
 use async_std::io;
-use libp2p::core::{self, transport::OptionalTransport};
 use libp2p::multiaddr::Protocol;
-use libp2p::Multiaddr;
 use std::borrow::Cow;
 use std::net::Ipv4Addr;
 
 // This is lifted from the rust libp2p-rs gossipsub and massaged to work with wasm.
 // The "glue" to get messages from the browser injected into this service isn't done yet.
-pub fn service(
+pub async fn service(
     wasm_external_transport: Option<wasm_ext::ExtTransport>,
     dial: Option<String>,
-) -> impl Future<Output = ()> {
+) {
     // Create a random PeerId
     let local_key = identity::Keypair::generate_ed25519();
     let local_peer_id = PeerId::from(local_key.public());
@@ -43,30 +45,42 @@ pub fn service(
 
     #[cfg(not(target_os = "unknown"))]
     let transport = transport.or_transport({
-        let desktop_trans = tcp::TcpConfig::new();
-        let desktop_trans =
-            websocket::WsConfig::new(desktop_trans.clone()).or_transport(desktop_trans);
-        OptionalTransport::some(
-            if let Ok(dns) = dns::DnsConfig::new(desktop_trans.clone()) {
-                dns.boxed()
-            } else {
-                desktop_trans.map_err(dns::DnsErr::Underlying).boxed()
-            },
-        )
+        let tcp_transport = tcp::TcpTransport::new(tcp::GenTcpConfig::new().nodelay(true));
+        let ws_tcp = websocket::WsConfig::new(tcp::TcpTransport::new(
+            tcp::GenTcpConfig::new().nodelay(true),
+        ))
+        .or_transport(tcp_transport);
+
+        OptionalTransport::some(if let Ok(dns) = dns::DnsConfig::system(ws_tcp).await {
+            dns.boxed()
+        } else {
+            websocket::WsConfig::new(tcp::TcpTransport::new(
+                tcp::GenTcpConfig::new().nodelay(true),
+            ))
+            .or_transport(tcp::TcpTransport::new(
+                tcp::GenTcpConfig::new().nodelay(true),
+            ))
+            .map_err(dns::DnsErr::Transport)
+            .boxed()
+        })
     });
 
-    let transport = transport
+    let noise_keys = noise::Keypair::<noise::X25519Spec>::new()
+        .into_authentic(&local_key)
+        .expect("Signing libp2p-noise static DH keypair failed.");
+
+    let transport: core::transport::Boxed<(PeerId, core::muxing::StreamMuxerBox)> = transport
         .upgrade(core::upgrade::Version::V1)
-        .authenticate(secio::SecioConfig::new(local_key))
+        .authenticate(noise::NoiseConfig::xx(noise_keys).into_authenticated())
         .multiplex(core::upgrade::SelectUpgrade::new(
-            yamux::Config::default(),
-            mplex::MplexConfig::new(),
+            yamux::YamuxConfig::default(),
+            mplex::MplexConfig::default(),
         ))
-        .map(|(peer, muxer), _| (peer, core::muxing::StreamMuxerBox::new(muxer)))
-        .timeout(std::time::Duration::from_secs(20));
+        .timeout(std::time::Duration::from_secs(20))
+        .boxed();
 
     // Create a Gossipsub topic
-    let topic = Topic::new("rnges.us".into());
+    let topic = Topic::new("test");
 
     // Create a Swarm to manage peers and events
     let mut swarm = {
@@ -77,18 +91,31 @@ pub fn service(
         let message_id_fn = |message: &GossipsubMessage| {
             let mut s = DefaultHasher::new();
             message.data.hash(&mut s);
-            MessageId(s.finish().to_string())
+            MessageId::from(s.finish().to_string())
         };
 
         // set custom gossipsub
-        let gossipsub_config = gossipsub::GossipsubConfigBuilder::new()
+        let gossipsub_config = gossipsub::GossipsubConfigBuilder::default()
             .heartbeat_interval(Duration::from_secs(10))
+            .validation_mode(ValidationMode::Strict)
             .message_id_fn(message_id_fn) // content-address messages. No two messages of the
             //same content will be propagated.
-            .build();
+            .build()
+            .expect("valid config");
         // build a gossipsub network behaviour
-        let mut gossipsub = gossipsub::Gossipsub::new(local_peer_id.clone(), gossipsub_config);
-        gossipsub.subscribe(topic.clone());
+        let mut gossipsub: gossipsub::Gossipsub =
+            gossipsub::Gossipsub::new(MessageAuthenticity::Signed(local_key), gossipsub_config)
+                .expect("Correct configuration");
+        gossipsub.subscribe(&topic).unwrap();
+
+        // Reach out to another node if specified
+        if let Some(to_dial) = dial {
+            match to_dial.parse() {
+                Ok(id) => gossipsub.add_explicit_peer(&id),
+                Err(err) => println!("Failed to parse explicit peer id: {:?}", err),
+            }
+        }
+
         libp2p::Swarm::new(transport, gossipsub, local_peer_id)
     };
 
@@ -98,31 +125,23 @@ pub fn service(
     let listen_addr = Multiaddr::empty()
         .with(Protocol::from(Ipv4Addr::UNSPECIFIED))
         .with(Protocol::Tcp(38615))
-        .with(Protocol::Ws(std::borrow::Cow::Borrowed("/")));
+        .with(Protocol::Ws(Cow::Borrowed("/")));
     libp2p::Swarm::listen_on(&mut swarm, listen_addr).unwrap();
-
-    // Reach out to another node if specified
-    if let Some(to_dial) = dial {
-        let dialing = to_dial.clone();
-        match to_dial.parse() {
-            Ok(to_dial) => match libp2p::Swarm::dial_addr(&mut swarm, to_dial) {
-                Ok(_) => println!("Dialed {:?}", dialing),
-                Err(e) => println!("Dial {:?} failed: {:?}", dialing, e),
-            },
-            Err(err) => println!("Failed to parse address to dial: {:?}", err),
-        }
-    }
 
     // Read full lines from stdin (Disable for wasm, there is no stdin)
     #[cfg(not(target_os = "unknown"))]
-    let mut stdin = io::BufReader::new(io::stdin()).lines();
+    let mut stdin = io::BufReader::new(io::stdin()).lines().fuse();
 
     let mut listening = false;
+
     future::poll_fn(move |cx: &mut Context| {
         #[cfg(not(target_os = "unknown"))]
         loop {
             match stdin.try_poll_next_unpin(cx) {
-                Poll::Ready(Some(Ok(line))) => swarm.publish(&topic, line.as_bytes()),
+                Poll::Ready(Some(Ok(line))) => swarm
+                    .behaviour_mut()
+                    .publish(topic.clone(), line.as_bytes())
+                    .unwrap(),
                 Poll::Ready(Some(Err(_))) => panic!("Stdin errored"),
                 Poll::Ready(None) => panic!("Stdin closed"),
                 Poll::Pending => break,
@@ -132,7 +151,11 @@ pub fn service(
         loop {
             match swarm.poll_next_unpin(cx) {
                 Poll::Ready(Some(gossip_event)) => match gossip_event {
-                    GossipsubEvent::Message(peer_id, id, message) => log::info!(
+                    SwarmEvent::Behaviour(GossipsubEvent::Message {
+                        propagation_source: peer_id,
+                        message_id: id,
+                        message,
+                    }) => log::info!(
                         "Got message: {} with id: {} from peer: {:?}",
                         String::from_utf8_lossy(&message.data),
                         id,
@@ -151,6 +174,7 @@ pub fn service(
             }
         }
 
-        Poll::Pending
+        Poll::<String>::Pending
     })
+    .await;
 }
